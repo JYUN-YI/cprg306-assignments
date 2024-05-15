36cb57337db0c81dac3af9cd018ad089abe354b0
Jyun-Yi Lin <dazeiihaze@gmail.com>
2024-05-15 10:13:08 -0600

    Upload student-info.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function StudentInfo() {
+    return (
+        <main> 
+        <h1>Jyun Yi Lin</h1> 
+        <Link href = "https://github.com/JYUN-YI/cprg306-assignments.git">https://github.com</Link>
+        </main> 
+    )
+}
